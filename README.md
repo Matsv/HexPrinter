6134e16c05dbc4b2979c446f7978cc636ac526dc
Matsv <admin@matsv.nl>
2016-05-03 20:38:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#Just an easy Hex printer, didn't want to calculate it myself
+
+##Installation
+```
+    Clone this repo
+
+    mvn clean package
+
+    Enjoy the jar file in the target directory
+
+```
+
+##Usage
+```
+    java -jar HexPrinter.jar (integers)
+
+    Example:
+
+    java -jar HexPrinter.jar 45 12 64
---
+++ src/main/java/nl/Matsv/HexPrinter/Print.java
@@ -0,0 +1,26 @@
+package nl.Matsv.HexPrinter;
+
+/**
+ * Created by Mats on 3-5-16.
+ */
+public class Print {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("No args given");
+            exit(1);
+        }
+
+        for (String s : args){
+            try {
+                Byte i = Byte.parseByte(s);
+                System.out.println(String.format("%d => 0x%02X", i, i));
+            } catch (Exception e){
+                System.out.println("ERROR: " + e.getLocalizedMessage());
+            }
+        }
+    }
+
+    private static void exit(int num){
+        System.exit(num);
+    }
+}
